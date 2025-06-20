4452ff8c679c38407d6be0404afb2d6580664507
Muzammil-EDF <123097477+Muzammil-EDF@users.noreply.github.com>
2025-06-20 13:02:34 +0500

    add send file import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
